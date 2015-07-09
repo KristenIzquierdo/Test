230aab5dd31a9cef12dca3c5a36b96bd0fd1701a
Kristen <krisemi925@gmail.com>
2015-07-09 13:43:50 +0000

    pass all tests

---
+++ holiday_greeting.rb
@@ -0,0 +1,5 @@
+def holiday(holiday = "Mother's Day", name = "Mom", person = "From Your Favorite Child")
+  puts "Happy #{holiday}, #{name} - From #{person}"
+end
+puts holiday
+puts holiday("Fourth of July", "Beyoncé", "Jay-Z")
--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,18 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-
-
 ## Call your method below - use puts to see it in the terminal!
+def mothers_day
+  puts "Happy Mother's Day, Mom!"
+end
+puts mothers_day
 
+def mothers_day(name)
+  puts "Happy Mother's Day, #{name}!"
+end
+puts mothers_day("Ileana")
+
+def mothers_day(name = "Mom")
+  puts "Happy Mother's Day, #{name}!"
+end
+puts mothers_day
+puts mothers_day("Beyoncé")
